5cfd9df837c003641a335006dd5c4a5458c12cf2
Liu SL <songlin0305@gmail.com>
2021-12-12 17:30:52 +0800

    1st commit sts

---
+++ src/main/java/sg/edu/nus/u/CreateDemoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/u/CreateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
